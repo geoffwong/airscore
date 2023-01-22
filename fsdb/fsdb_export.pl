6fc875ef36e448ada8e843bff17725e925b57198
Geoff Wong <geoff.wong@hydrix.com>
2023-01-22 15:47:27 +1100

    Update export .. now can be imported by task results don't show up 'NYP' for some reason.

--- fsdb/fsdb_export.pl
+++ fsdb/fsdb_export.pl
@@ -123,19 +123,19 @@ $dbh = db_connect();
 my $comp = read_competition($comPk);
 $fsdb->{'FsCompetition'}->{'id'} = $comp->{'comPk'};
 $fsdb->{'FsCompetition'}->{'name'} = $comp->{'name'};
-$fsdb->{'FsCompetition'}->{'location'} = $comp->{'comLocation'};
+$fsdb->{'FsCompetition'}->{'location'} = $comp->{'location'};
 $fsdb->{'FsCompetition'}->{'from'} = substr($comp->{'datefrom'},0,10);
 $fsdb->{'FsCompetition'}->{'to'} = substr($comp->{'dateto'},0,10);
 $utc = $comp->{'timeoffset'};
 $fsdb->{'FsCompetition'}->{'utc_offset'} = $utc;
-$fsdb->{'FsCompetition'}->{'discipline'} = 'paragliding';
+$fsdb->{'FsCompetition'}->{'discipline'} = uc($comp->{'class'});
 if ($ref->{'comOverallScore'} ne 'ftv')
 {
     $fsdb->{'FsCompetition'}->{'ftv_factor'} = '1';
 }
 else
 {
-    $fsdb->{'FsCompetition'}->{'ftv_factor'} = $comp->{'overallparam'} / 100;
+    $fsdb->{'FsCompetition'}->{'ftv_factor'} = 1 - ($comp->{'overallparam'} / 100);
 }
 $fsdb->{'FsCompetition'}->{'fai_sanctioning'} = '2';
 $fsdb->{'FsCompetition'}->{'categories'} = 'filter';
@@ -146,27 +146,26 @@ $fsdb->{'FsCompetition'}->{'FsScoreFormula'} = \%formula;
 
 $ref = read_formula($comPk);
 $comformula = $ref;
-$formula{'id'} = 'OzGAP2018';
+$formula{'id'} = uc($ref->{'class'}) . $ref->{'version'};
 $formula{'use_distance_points'} = '1';
 $formula{'use_time_points'} = '1';
 $formula{'use_departure_points'} = '0';
 $formula{'use_leading_points'} = '1';
-$formula{'nom_time'} = $ref->{'forNomTime'} / 60;
-$formula{'nom_goal'} = $ref->{'forNomGoal'} / 100;
-$formula{'time_points_if_not_in_goal'} = 1 - (0+$ref->{'forGoalSSPenalty'});
-$formula{'jump_the_gun_factor'} = '0';
+$formula{'nom_time'} = $ref->{'nomtime'} / 60;
+$formula{'nom_goal'} = $ref->{'nomgoal'} / 100;
+$formula{'time_points_if_not_in_goal'} = 1 - (0+$ref->{'sspenalty'});
 $formula{'use_1000_points_for_max_day_quality'} = '1';
 $formula{'time_validity_based_on_pilot_with_speed_rank'} = '1';
 
-$formula{'min_dist'} = $ref->{'forMinDist'};
-$formula{'nom_dist'} = $ref->{'forNomDist'};
-$formula{'nom_launch'} =$ref->{'forNomLaunch'};
+$formula{'min_dist'} = $ref->{'mindist'};
+$formula{'nom_dist'} = $ref->{'nomdist'};
+$formula{'nom_launch'} = $ref->{'nomlaunch'};
 $formula{'day_quality_override'} = "0";
-$formula{'bonus_gr'} = "4";
-$formula{'jump_the_gun_factor'} = "0";
-$formula{'jump_the_gun_max'} = "0";
+$formula{'bonus_gr'} = $ref->{'glidebonus'};
+$formula{'jump_the_gun_factor'} = '0';
+$formula{'jump_the_gun_max'} = '0';
 $formula{'normalize_1000_before_day_quality'} = '0';
-if ($ref->{'forGoalSSpenalty'} == 1.0)
+if ($ref->{'sspenalty'} == 1.0)
 {
     $formula{'time_points_if_not_in_goal'} = '0';
 }
@@ -195,7 +194,7 @@ else
 }
 $formula{'use_distance_points'} = '1';
 $formula{'use_distance_squared_for_LC'} = '0';
-if ($ref->{'forDeparture'} eq 'leadout')
+if ($ref->{'departure'} eq 'leadout')
 {
     $formula{'use_leading_points'} = '1';
 }
@@ -205,7 +204,7 @@ elsif ($ref->{'forDeparture'} eq 'departure')
 }
 $formula{'use_semi_circle_control_zone_for_goal_line'} = '1';
 $formula{'use_time_points'} = '1';
-$formula{'scoring_altitude'} =
+$formula{'scoring_altitude'} = 'GPS';
 $formula{'final_glide_decelerator'} = 'none';
 $formula{'no_final_glide_decelerator_reason'} = '';
 $formula{'min_time_span_for_valid_task'} = '60';
@@ -222,11 +221,11 @@ $formula{'use_constant_leading_weight'} = '0';
 $formula{'use_pwca2019_for_lc'} = '0';
 $formula{'use_flat_decline_of_timepoints'} = '0';
 
-$intformula{'arrival_weight'} = $ref->{'forWeightArrival'};
+$intformula{'arrival_weight'} = $ref->{'weightarrival'};
 $intformula{'departure_weight'} = 0;
-$intformula{'leading_weight'} = $ref->{'forWeightStart'}; 
-$intformula{'time_weight'} = $ref->{'forWeightSpeed'};
-$intformula{'distance_weight'} = 1 - $ref->{'forWeightStart'} - $ref->{'forWeightSpeed'};
+$intformula{'leading_weight'} = $ref->{'weightstart'}; 
+$intformula{'time_weight'} = $ref->{'weightspeed'};
+$intformula{'distance_weight'} = 1 - $ref->{'weightstart'} - $ref->{'weightspeed'};
 
 $fsdb->{'FsCompetition'}->{'FsParticipants'}->{'FsParticipant'} = \@pilots;
 $sth = $dbh->prepare("select P.* from tblPilot P, tblTaskResult TR, tblTrack TK, tblTask T where P.pilPk=TK.pilPk and TK.traPk=TR.traPk and TR.tasPk=T.tasPk and T.comPk=$comPk group by P.pilPk");
@@ -266,6 +265,10 @@ while (defined($ref))
 }
 
 $count = 1;
+my $rankings = emarr();
+my $participants = emarr();
+$fsdb->{'FsCompetition'}->{'FsCompetitionResults'} = $rankings;
+$task->{'FsParticipants'}->{'FsParticipant'} = $participants;
 $fsdb->{'FsCompetition'}->{'FsTasks'}->{'FsTask'} = \@tasks;
 
 # Tasks
@@ -292,25 +295,32 @@ foreach my $tasPk (@alltasks)
     my @tps;
     $task = empty();
     $task->{'id'} = $count;
-    $task->{'name'} = $ref->{'tasName'};
-    $task->{'tracklog_folder'} =  '';
+    $task->{'name'} = $ref->{'name'};
+    $task->{'tracklog_folder'} =  'none';
     $task->{'FsScoreFormula'} = \%formula;
     #$task->{'FsParticipants'}->{'FsParticipant'} = \@pilots;
     #$task->{'FsTaskScoreParams'} = empty();
-    #$task->{'FsTaskScoreParams'}->{'ss_distance'} = ''
+    $task->{'FsTaskScoreParams'}->{'ss_distance'} = sprintf("%.2f", $ref->{'ssdistance'});
     $task->{'FsTaskScoreParams'}->{'task_distance'} = sprintf("%.2f", $ref->{'short_distance'});
+    $task->{'FsTaskScoreParams'}->{'launch_to_ess_distance'} = sprintf("%.2f", $ref->{'endssdistance'});
     $task->{'FsTaskScoreParams'}->{'no_of_pilots_present'} = $task_totals->{'pilots'};
     $task->{'FsTaskScoreParams'}->{'no_of_pilots_flying'} = $task_totals->{'launched'};
     $task->{'FsTaskScoreParams'}->{'no_of_pilots_lo'} = $task_totals->{'launched'} - $task_totals->{'goal'};
-    $task->{'FsTaskScoreParams'}->{'no_of_pilots_reaching_nom_dist'} = '';
+    $task->{'FsTaskScoreParams'}->{'no_of_pilots_reaching_nom_dist'} = $task_totals->{'launched'};	# fixme
     $task->{'FsTaskScoreParams'}->{'no_of_pilots_reaching_es'} = $task_totals->{'ess'};
     $task->{'FsTaskScoreParams'}->{'no_of_pilots_reaching_goal'} = $task_totals->{'goal'};
     $task->{'FsTaskScoreParams'}->{'no_of_pilots_in_competition'} = $task_totals->{'pilots'};
     $task->{'FsTaskScoreParams'}->{'sum_dist_over_min'} = sprintf("%.2f", $task_totals->{'distance'});
-    $task->{'FsTaskScoreParams'}->{'max_time_to_get_time_points'} = '';
+    $task->{'FsTaskScoreParams'}->{'max_time_to_get_time_points'} = '0';
     $task->{'FsTaskScoreParams'}->{'no_of_pilots_with_time_points'} = $task_totals->{'ess'};  # @fixme
     #$task->{'FsTaskScoreParams'}->{'k'} = '';
     #$task->{'FsTaskScoreParams'}->{'arrival_weight'} = 
+    $task->{'FsTaskScoreParams'}->{'best_dist'} = $task_totals->{'maxdist'};
+    $task->{'FsTaskScoreParams'}->{'best_time'} = $task_totals->{'fastest'};
+    $task->{'FsTaskScoreParams'}->{'worst_time'} = '0';
+    $task->{'FsTaskScoreParams'}->{'sum_flown_distance'} = $task_totals->{'avdist'} * $task_totals->{'launched'};
+    $task->{'FsTaskScoreParams'}->{'sum_dist_over_min'} = ($task_totals->{'avdist'} - $formula{'min_dist'}) * $task_totals->{'launched'};
+    $task->{'FsTaskScoreParams'}->{'sum_real_dist_over_min'} = $task->{'FsTaskScoreParams'}->{'sum_dist_over_min'}; # ??
     $intformula{'arrival_weight'} = $ref->{'forWeightArrival'};
     $task->{'FsTaskScoreParams'}->{'departure_weight'} = $intformula{'departure_weight'};
     $task->{'FsTaskScoreParams'}->{'leading_weight'} = $intformula{'leading_weight'};
@@ -327,9 +337,14 @@ foreach my $tasPk (@alltasks)
     $task->{'FsTaskScoreParams'}->{'time_validity'} = $time;
     $task->{'FsTaskScoreParams'}->{'launch_validity'} = $launch;
     $task->{'FsTaskScoreParams'}->{'distance_validity'} = $distance;
-    $task->{'FsTaskScoreParams'}->{'day_quality'} = $time * $launch * $distance;
+    $task->{'FsTaskScoreParams'}->{'stop_validity'} = $stopped;
+    $task->{'FsTaskScoreParams'}->{'day_quality'} = $time * $launch * $distance * $stopped;
+    $task->{'FsTaskScoreParams'}->{'ftv_day_validity'} = $time * $launch * $distance * $stopped;
+    $task->{'FsTaskScoreParams'}->{'time_points_stop_correction'} = 0;
+    # taskdisttotp list
     $taskmap{$ref->{'tasPk'}} = $task;
 
+    # fix - check if stopped
     $task->{'FsTaskState'} = empty();
     $task->{'FsTaskState'}->{'task_state'} = 'REGULAR';
     $task->{'FsTaskState'}->{'score_back_time'} = '5';
@@ -365,6 +380,8 @@ while (defined($ref))
         $task->{'FsTaskDefinition'}->{'goal'} = 'CIRCLE';
         $task->{'FsTaskDefinition'}->{'ss'} = $ss;
         $task->{'FsTaskDefinition'}->{'es'} = $es;
+        $task->{'FsTaskDefinition'}->{'groundstart'} = '0';
+        $task->{'FsTaskDefinition'}->{'qnh_settings'} = '1013.25';
         $task->{'FsTaskDefinition'}->{'FsTurnpoint'} = $tps;
         $task->{'FsTaskScoreParams'}->{'FsTaskDistToTp'} = $sroute;
         $tps = emarr();
@@ -376,16 +393,20 @@ while (defined($ref))
     $turn->{'id'} = $ref->{'rwpName'};
     $turn->{'lat'} = sprintf("%.5f", $ref->{'rwpLatDecimal'});
     $turn->{'lon'} = sprintf("%.5f", $ref->{'rwpLongDecimal'});
+    $turn->{'altitude'} = $ref->{'rwpAltitude'};
     $turn->{'radius'} = $ref->{'tawRadius'};
-    $turn->{'open'} = '';  # "2020-01-19T11:00:00-05:00"
-    $turn->{'close'} = ''; # "2020-01-19T11:00:00-05:00"
+    $turn->{'open'} = $ref->{'tasStartTime'};
+    $turn->{'close'} = $ref->{'tasFinishTime'};
     if ($ref->{'tawType'} eq 'start')
     {
         $ss = $cnt;
+        $turn->{'open'} = $ref->{'tasTaskStart'};
+        $turn->{'close'} = $ref->{'tasStartCloseTime'};
     }
-    if ($ref->{'tawType'} eq 'start')
+    if ($ref->{'tawType'} eq 'speed')
     {
         $ss = $cnt;
+        $turn->{'close'} = $ref->{'tasStartCloseTime'};
     }
     if ($ref->{'tawType'} eq 'endspeed')
     {
@@ -429,22 +450,48 @@ $task->{'FsTaskScoreParams'}->{'FsTaskDistToTp'} = $sroute;
 # Add start gates <FsStartGate open="">
 
 # Results
-my $taskr = empty();
-my $rarr = emarr();
+my $tpresult;
+my $taskr;
+my $taskresults = emarr();
+my $taskoverall = empty();
+my $partinfo = emarr();
+my $partresults = emarr();
 my $lastPk = 0;
+my $place = 1;
 
-$sth = $dbh->prepare("select TK.*, TR.*, TL.pilPk, TL.traStart, date_add(TK.tasDate, INTERVAL TR.tarSS SECOND) as Sss, date_add(TK.tasDate, INTERVAL TR.tarES SECOND) as Ess from tblTaskResult TR, tblTask TK, tblTrack TL  where TR.tasPk=TK.tasPk and TL.traPk=TR.traPk and TK.comPk=$comPk order by TK.tasPk");
+$taskoverall->{'id'} = 'overall';
+$taskoverall->{'title'} = 'Overall';
+#$taskoverall->{'ts'} = $ref->{'finish'};
+$taskoverall->{'result_pattern'} = '#0';
+$taskoverall->{'FsTaskResultParticipants'} = $partresults;
+push @$taskresults, $taskoverall;
+
+$sth = $dbh->prepare("select TK.*, TR.*, TL.pilPk, TL.traStart, date_add(TK.tasDate, INTERVAL TR.tarSS SECOND) as Sss, date_add(TK.tasDate, INTERVAL TR.tarES SECOND) as Ess from tblTaskResult TR, tblTask TK, tblTrack TL  where TR.tasPk=TK.tasPk and TL.traPk=TR.traPk and TK.comPk=$comPk order by TK.tasPk, TR.tarScore desc");
 $sth->execute();
 $ref = $sth->fetchrow_hashref();
 while (defined($ref))
 {
     if (($lastPk != $ref->{'tasPk'}) && ($lastPk != 0))
     {
-        # insert into tasks
+        # insert results into tasks
         $task = $taskmap{$lastPk};
-        $task->{'FsParticipants'}->{'FsParticipant'} = $rarr;
+        $task->{'FsParticipants'}->{'FsParticipant'} = $partinfo;
+        $task->{'FsTaskResults'} = $taskresults;
+        $taskoverall->{'FsTaskScoreParams'} = $task->{'FsTaskScoreParams'};
+        $taskoverall->{'ts'} = $task->{'finish'};
+        # clean up for new task
         $taskr = empty();
-        $rarr = emarr();
+        $taskresults = emarr();
+        $taskoverall = empty();
+        $partinfo = emarr();
+        $partresults = emarr();
+        $taskoverall->{'id'} = 'overall';
+        $taskoverall->{'title'} = 'Overall';
+        $taskoverall->{'result_pattern'} = '#0';
+        $taskoverall->{'FsTaskResultParticipants'} = $partresults;
+        push @$taskresults, $taskoverall;
+        $participants = emarr();
+        $place = 1;
     }
     $lastPk = $ref->{'tasPk'};
     $taskr = empty();
@@ -462,13 +509,13 @@ while (defined($ref))
         $taskr->{'FsFlightData'}->{'ss_time'} = '';
     }
     $taskr->{'FsFlightData'}->{'finished_task'} = $ref->{'tarGoal'};
-    $taskr->{'FsFlightData'}->{'tracklog_filename'} = '';
+    $taskr->{'FsFlightData'}->{'tracklog_filename'} = 'none.igc';
     $taskr->{'FsFlightData'}->{'lc'} = sprintf("%.1f", $ref->{'tarLeadingCoeff'});
     $taskr->{'FsFlightData'}->{'iv'} = 0;
     $taskr->{'FsFlightData'}->{'ts'} = fs_time($ref->{'traStart'}, 0);
     $taskr->{'FsResult'} = empty();
-    $taskr->{'FsResult'}->{'rank'} = $ref->{'tarPlace'};
-    $taskr->{'FsResult'}->{'finished_ss_rank'} = $ref->{'tarPlace'};
+    $taskr->{'FsResult'}->{'rank'} = $place;
+    $taskr->{'FsResult'}->{'finished_ss_rank'} = $place;    # NFI
     $taskr->{'FsResult'}->{'points'} = sprintf("%.0f", $ref->{'tarScore'});
     $taskr->{'FsResult'}->{'distance_points'} = sprintf("%.1f", $ref->{'tarDistanceScore'});
     $taskr->{'FsResult'}->{'time_points'} = sprintf("%.1f", $ref->{'tarSpeedScore'});
@@ -479,15 +526,75 @@ while (defined($ref))
     $taskr->{'FsResult'}->{'penalty_points'} = $ref->{'tarPenalty'};
     $taskr->{'FsResult'}->{'penalty_reason'} = '';
     $taskr->{'FsResult'}->{'ss_time_dec_hours'} = '';
-    $taskr->{'FsResult'}->{'ts'} = '';
+    $taskr->{'FsResult'}->{'ts'} = fs_time($ref->{'Ess'}, 0); # now()?
+
+    ## add overall task result ordering
+    # <FsTaskResult id="overall" title="Overall" ts="2020-05-06T21:57:00+02:00" result_pattern="#0">
+    #   FsTaskScoreParams (as above) + [ FsTaskDistToTp ] 
+    #   FsTaskResultParticipants = emarr(); # [ FsTaskParticipant ]  (in ranked order)
+    #   <FsTaskParticipantResult id="422" rank="1" distance_points="389.1" departure_points="0" arrival_points="0" leading_points="95.8" time_points="504" points="989" /> *
+    # Mindless duplication of data
+    $tpresult = empty();
+    $tpresult->{'id'} = $taskr->{'id'};
+    $tpresult->{'rank'} = $place;
+    $tpresult->{'distance_points'} = $taskr->{'FsResult'}->{'distance_points'};
+    $tpresult->{'departure_points'} =$taskr->{'FsResult'}->{'departure_points'};
+    $tpresult->{'arrival_points'} = $taskr->{'FsResult'}->{'arrival_points'};
+    $tpresult->{'leading_points'} = $taskr->{'FsResult'}->{'leading_points'};
+    $tpresult->{'time_points'} = $taskr->{'FsResult'}->{'time_points'};
+    $tpresult->{'points'} = $taskr->{'FsResult'}->{'points'};
+    push @$partresults, $tpresult;
+
+    $place++;
     
     # push on tasks results
-    push @$rarr, $taskr;
+    push @$partinfo, $taskr;
 
     $ref = $sth->fetchrow_hashref();
 }
 $task = $taskmap{$lastPk};
-$task->{'FsParticipants'}->{'FsParticipant'} = $rarr;
+$task->{'FsParticipants'}->{'FsParticipant'} = $partinfo;
+$task->{'FsTaskResults'} = $taskresults;
+$taskoverall->{'FsTaskScoreParams'} = $task->{'FsTaskScoreParams'};
+$taskoverall->{'ts'} = $task->{'finish'};
+
+# results
+my %overall;
+
+$overall{'id'} = 'overall';
+$overall{'title'} = 'Overall';
+$overall{'top'} = 'all';
+$overall{'ts'} = '0';   # timestamp ..
+$overall{'task_result_pattern'} = '#0';
+$overall{'comp_result_pattern'} = '#0';
+$overall{'comp_result_pattern'} = '#0';
+$overall{'FsParticipant'} = emarr();
+push @$rankings, \%overall;
+
+#    rankings = result['rankings']
+#    r = result['results']
+#    for el in rankings:
+#        rank_id = el['rank_id']
+#        cr = ET.SubElement(compresults, 'FsCompetitionResult')
+#        cr.set('id', str(el['rank_name']).lower())
+#        cr.set('title', str(el['rank_name']))
+#        cr.set('top', 'all')  # ?
+#        cr.set('tasks', ';'.join([str(i) for i in task_ids.keys()]))
+#        cr.set('ts', '')
+#        cr.set('task_result_pattern', '#0.0' if self.comp.formula.task_result_decimal == 1 else '#0')
+#        cr.set('comp_result_pattern', '#0.0' if self.comp.formula.comp_result_decimal == 1 else '#0')
+#        for p in [x for x in r if x['rankings'][rank_id]]:
+#            pr = ET.SubElement(cr, 'FsParticipant')
+#            pr.set('id', str(p['ID']))
+#            pr.set('points', p['score'].split('>')[1].split('<')[0])
+#            pr.set('rank', str(p['rankings'][rank_id]).split(' ')[0])
+#            res = list(p['results'].items())
+#            for x in res:
+#                pt = ET.SubElement(pr, 'FsTask')
+#                pt.set('id', str(next(k for k, v in task_ids.items() if v == x[0])))
+#                pt.set('points', x[1]['pre'])
+#                pt.set('counting_points', x[1]['score'])
+#                pt.set('counts', '1')
 
 
 #print Dumper(\%fsx);
